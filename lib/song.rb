e8773f18c9af5847bf9c608daedc3aa4982d620a
Francis Mendez <fmendez884@gmail.com>
2019-06-10 03:20:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -44,7 +44,8 @@ class Song
   
   def self.new_from_filename(filename)
     song = self.new
-    binding.pry
+    song.name = filename.split(/[-.]/)[0]
+    song.artist_name = filename.split(/[-.]/)[1]
   end 
   
   def self.create_from_filename(filename)
